b9a06dec182270e69b6751a9ed3ef8a15b69c372
Tran Quoc Viet <TranQuocViet@gmail.com>
2026-03-27 23:03:05 +0700

    Bai tap lab01

---
+++ Lab01/ArrayProgram.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class ArrayProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println("Sorted array:");
+        for (int x : arr)
+            System.out.print(x + " ");
+
+        double avg = (double) sum / n;
+        System.out.println("\nSum = " + sum);
+        System.out.println("Average = " + avg);
+    }
+}
