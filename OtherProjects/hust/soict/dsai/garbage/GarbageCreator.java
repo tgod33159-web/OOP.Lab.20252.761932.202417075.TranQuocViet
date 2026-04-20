d42a0ee5ca94188caa9aa7c5abee279653ea5a9d
Viet Quoc <tgod33159@gmail.com>
2026-04-20 23:39:17 +0700

    garbage-test

---
+++ OtherProjects/hust/soict/dsai/garbage/GarbageCreator.java
@@ -0,0 +1,26 @@
+package hust.soict.dsai.garbage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GarbageCreator {
+    public static void main(String[] args) {
+        String filename = "test.exe"; // Hãy chọn một file lớn trong máy bạn
+        byte[] inputBytes = { 0 };
+        long startTime, endTime;
+
+        try {
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+            startTime = System.currentTimeMillis();
+            String outputString = "";
+            for (byte b : inputBytes) {
+                outputString += (char) b;
+            }
+            endTime = System.currentTimeMillis();
+            System.out.println("Time with +: " + (endTime - startTime) + "ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ OtherProjects/hust/soict/dsai/garbage/NoGarbage.java
@@ -0,0 +1,27 @@
+package hust.soict.dsai.garbage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class NoGarbage {
+    public static void main(String[] args) {
+        String filename = "test.exe";
+        byte[] inputBytes = { 0 };
+        long startTime, endTime;
+
+        try {
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+            startTime = System.currentTimeMillis();
+            StringBuilder outputStringBuilder = new StringBuilder();
+            for (byte b : inputBytes) {
+                outputStringBuilder.append((char) b);
+            }
+            String outputString = outputStringBuilder.toString();
+            endTime = System.currentTimeMillis();
+            System.out.println("Time with StringBuilder: " + (endTime - startTime) + "ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
