41fa98b52d375d1becc578cef7804eb476ab3624
Viet Quoc <tgod33159@gmail.com>
2026-04-20 22:43:57 +0700

    passing-parameter

--- AimsProject/Sourcecode/DigitalVideoDisc.java
+++ AimsProject/Sourcecode/DigitalVideoDisc.java
@@ -1,32 +1,25 @@
 public class DigitalVideoDisc {
-
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
-
-    // Constructors
     public DigitalVideoDisc() {
     }
-
     public DigitalVideoDisc(String title) {
         this.title = title;
     }
-
     public DigitalVideoDisc(String category, String title, float cost) {
         this.category = category;
         this.title = title;
         this.cost = cost;
     }
-
     public DigitalVideoDisc(String director, String category, String title, float cost) {
         this.director = director;
         this.category = category;
         this.title = title;
         this.cost = cost;
     }
-
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
         this.category = category;
@@ -34,24 +27,37 @@ public class DigitalVideoDisc {
         this.length = length;
         this.cost = cost;
     }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-    // Getters
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
     public String getTitle() {
         return title;
     }
-
     public String getCategory() {
         return category;
     }
-
     public String getDirector() {
         return director;
     }
-
     public int getLength() {
         return length;
     }
-
     public float getCost() {
         return cost;
     }
--- AimsProject/Sourcecode/TestPassingParameter.java
+++ AimsProject/Sourcecode/TestPassingParameter.java
@@ -1,20 +1,26 @@
 public class TestPassingParameter {
-
     public static void main(String[] args) {
-
         DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
-
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+        System.out.println("--- Test Swap ---");
         System.out.println("Before swap:");
         System.out.println("jungleDVD title: " + jungleDVD.getTitle());
-
+        System.out.println("cinderellaDVD title: " + cinderellaDVD.getTitle());
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("After swap:");
+        System.out.println("jungleDVD title: " + jungleDVD.getTitle());
+        System.out.println("cinderellaDVD title: " + cinderellaDVD.getTitle());
+        System.out.println("\n--- Test Change Title ---");
         changeTitle(jungleDVD, "New Jungle");
-
-        System.out.println("After change:");
+        System.out.println("After changeTitle:");
         System.out.println("jungleDVD title: " + jungleDVD.getTitle());
     }
-
+    public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        String oldTitle = dvd1.getTitle();
+        dvd1.setTitle(dvd2.getTitle());
+        dvd2.setTitle(oldTitle);
+    }
     public static void changeTitle(DigitalVideoDisc dvd, String title) {
-        dvd = new DigitalVideoDisc(title);
-        System.out.println("Inside function: " + dvd.getTitle());
+        dvd.setTitle(title); 
     }
 }
