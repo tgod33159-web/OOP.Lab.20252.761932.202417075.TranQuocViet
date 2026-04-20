aac592cc43a214a7907e099c85ac91161330e9ee
Viet Quoc <tgod33159@gmail.com>
2026-04-20 22:52:27 +0700

    class-members

--- AimsProject/Sourcecode/DigitalVideoDisc.java
+++ AimsProject/Sourcecode/DigitalVideoDisc.java
@@ -4,21 +4,31 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
+    private static int nbDigitalVideoDiscs = 0; 
+    private int id; 
     public DigitalVideoDisc() {
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title) {
         this.title = title;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String category, String title, float cost) {
         this.category = category;
         this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String director, String category, String title, float cost) {
         this.director = director;
         this.category = category;
         this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
@@ -26,39 +36,20 @@ public class DigitalVideoDisc {
         this.director = director;
         this.length = length;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
-    public void setTitle(String title) {
-        this.title = title;
+    public int getId() {
+        return id;
     }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
-    }
-
-    public void setCost(float cost) {
-        this.cost = cost;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public String getCategory() {
-        return category;
-    }
-    public String getDirector() {
-        return director;
-    }
-    public int getLength() {
-        return length;
-    }
-    public float getCost() {
-        return cost;
-    }
+    public void setTitle(String title) { this.title = title; }
+    public void setCategory(String category) { this.category = category; }
+    public void setDirector(String director) { this.director = director; }
+    public void setLength(int length) { this.length = length; }
+    public void setCost(float cost) { this.cost = cost; }
+    public String getTitle() { return title; }
+    public String getCategory() { return category; }
+    public String getDirector() { return director; }
+    public int getLength() { return length; }
+    public float getCost() { return cost; }
 }
