9e8d751a354bc191034d155cc9c017fae57744e6
Viet Quoc <tgod33159@gmail.com>
2026-04-20 23:09:20 +0700

    Complete Lab 06: Implement toString, isMatch and Search functions

--- AimsProject/Sourcecode/Cart.java
+++ AimsProject/Sourcecode/Cart.java
@@ -2,6 +2,7 @@ public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
     private DigitalVideoDisc[] itemsOrdered = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
     private int qtyOrdered = 0;
+
     public void addDigitalVideoDisc(DigitalVideoDisc disc) {
         if (qtyOrdered < MAX_NUMBERS_ORDERED) {
             itemsOrdered[qtyOrdered] = disc;
@@ -11,6 +12,7 @@ public class Cart {
             System.out.println("The cart is almost full");
         }
     }
+
     public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
         for (DigitalVideoDisc disc : dvdList) {
             if (qtyOrdered < MAX_NUMBERS_ORDERED) {
@@ -23,18 +25,20 @@ public class Cart {
             }
         }
     }
+
     public void addDigitalVideoDisc(DigitalVideoDisc... dvds) {
-    for (DigitalVideoDisc disc : dvds) {
-    if (qtyOrdered < MAX_NUMBERS_ORDERED) {
-    itemsOrdered[qtyOrdered] = disc;
-    qtyOrdered++;
-    System.out.println("The disc \"" + disc.getTitle() + "\" has been added");
-    } else {
-    System.out.println("The cart is full.");
-    break;
+        for (DigitalVideoDisc disc : dvds) {
+            if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+                itemsOrdered[qtyOrdered] = disc;
+                qtyOrdered++;
+                System.out.println("The disc \"" + disc.getTitle() + "\" has been added");
+            } else {
+                System.out.println("The cart is full.");
+                break;
+            }
+        }
     }
-    }
-    }
+
     public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
         if (qtyOrdered + 1 < MAX_NUMBERS_ORDERED) {
             itemsOrdered[qtyOrdered] = dvd1;
@@ -46,6 +50,7 @@ public class Cart {
             System.out.println("The cart is full, cannot add two discs.");
         }
     }
+
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
         boolean found = false;
         for (int i = 0; i < qtyOrdered; i++) {
@@ -64,6 +69,7 @@ public class Cart {
             System.out.println("The disc was not found");
         }
     }
+
     public float totalCost() {
         float sum = 0;
         for (int i = 0; i < qtyOrdered; i++) {
@@ -71,13 +77,52 @@ public class Cart {
         }
         return sum;
     }
-    public void printCart() {
-        System.out.println("***********CART***********");
+
+    public void print() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:");
         for (int i = 0; i < qtyOrdered; i++) {
-            System.out.println((i + 1) + ". " + itemsOrdered[i].getTitle()
-                    + " - $" + itemsOrdered[i].getCost());
+            System.out.println((i + 1) + ". " + itemsOrdered[i].toString());
         }
-        System.out.println("Total cost: $" + totalCost());
-        System.out.println("**************************");
+        System.out.println("Total cost: " + totalCost() + " $");
+        System.out.println("***************************************************");
+    }
+
+    public void searchById(int id) {
+        boolean found = false;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].getId() == id) {
+                System.out.println("Found match for ID " + id + ": " + itemsOrdered[i].toString());
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("No match found for ID: " + id);
+        }
+    }
+
+    public void searchByTitle(String title) {
+        boolean found = false;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].isMatch(title)) {
+                System.out.println("Found match for title \"" + title + "\": " + itemsOrdered[i].toString());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("No match found for title: " + title);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cart contents (").append(qtyOrdered).append(" items):\n");
+        for (int i = 0; i < qtyOrdered; i++) {
+            sb.append("\t").append(itemsOrdered[i].toString()).append("\n");
+        }
+        sb.append("Total cost: ").append(totalCost()).append(" $");
+        return sb.toString();
     }
 }
---
+++ AimsProject/Sourcecode/CartTest.java
@@ -0,0 +1,21 @@
+public class CartTest {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", 
+                "Animation", "Roger Allers", 87, 19.95f);
+        cart.addDigitalVideoDisc(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", 
+                "Science Fiction", "George Lucas", 87, 24.95f);
+        cart.addDigitalVideoDisc(dvd2);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", 
+                "Animation", 18.99f);
+        cart.addDigitalVideoDisc(dvd3);
+        cart.print();
+        System.out.println("\n--- Search by ID ---");
+        cart.searchById(1); 
+        cart.searchById(5); 
+        System.out.println("\n--- Search by Title ---");
+        cart.searchByTitle("Star Wars"); 
+        cart.searchByTitle("Cinderella"); 
+    }
+}
